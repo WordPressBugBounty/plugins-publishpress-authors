57cff4d7333bf4f2b19314741b66a0cda7914135
Sean Murphy <sean@iamseanmurphy.com>
2026-02-26 15:29:09 +0000

    4.12.0

--- publishpress-authors/lib/vendor/composer/installed.php
+++ publishpress-authors/lib/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => 'v4.11.0',
-        'version' => '4.11.0.0',
-        'reference' => 'cc367a12ee093df4f9e42ce5f6e64cbf6b08ee5e',
+        'pretty_version' => 'v4.12.0',
+        'version' => '4.12.0.0',
+        'reference' => 'b35c87fc72b084ad2f121dd5709de29e530cee96',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => 'v4.11.0',
-            'version' => '4.11.0.0',
-            'reference' => 'cc367a12ee093df4f9e42ce5f6e64cbf6b08ee5e',
+            'pretty_version' => 'v4.12.0',
+            'version' => '4.12.0.0',
+            'reference' => 'b35c87fc72b084ad2f121dd5709de29e530cee96',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
--- publishpress-authors/vendor/composer/installed.php
+++ publishpress-authors/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'publishpress/publishpress-authors',
-        'pretty_version' => 'v4.11.0',
-        'version' => '4.11.0.0',
-        'reference' => 'cc367a12ee093df4f9e42ce5f6e64cbf6b08ee5e',
+        'pretty_version' => 'v4.12.0',
+        'version' => '4.12.0.0',
+        'reference' => 'b35c87fc72b084ad2f121dd5709de29e530cee96',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'publishpress/publishpress-authors' => array(
-            'pretty_version' => 'v4.11.0',
-            'version' => '4.11.0.0',
-            'reference' => 'cc367a12ee093df4f9e42ce5f6e64cbf6b08ee5e',
+            'pretty_version' => 'v4.12.0',
+            'version' => '4.12.0.0',
+            'reference' => 'b35c87fc72b084ad2f121dd5709de29e530cee96',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
